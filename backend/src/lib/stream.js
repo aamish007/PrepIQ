adffced7a39236f8780bb3a63cfa1f918490b223
aamish007 <aamishhussain7@gmail.com>
2026-04-06 03:03:38 +0530

    authentication plus routes created

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,30 @@
+import {StreamChat} from 'stream-chat'
+import {ENV} from './env.js'
+// import { use } from 'react'
+const apiKey=ENV.STREAM_API_KEY
+const apiSecret=ENV.STREAM_API_SECRET
+
+if(!apiKey || !apiSecret){
+    console.error("Stream API Key or secret is missing.")
+}
+export const chatClient=StreamChat.getInstance(apiKey,apiSecret);
+
+export const upsertStreamUser= async(userData)=>{
+    try{
+        await chatClient.upsertUser(userData)
+        console.log("User upserted to Stream: ", userData)
+    }
+    catch(error){
+        console.error("Error upserting user to Stream:",error)
+    }
+}
+
+export const deleteStreamUser= async(userId)=>{
+    try{
+        await chatClient.deleteUser(userId)
+        console.log("User deleted from Stream: ", userId)
+    }
+    catch(error){
+        console.error("Error deleting user from Stream:",error)
+    }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,12 +5,16 @@ import cors from "cors";
 import { connectDB } from "./lib/db.js";
 import { serve } from "inngest/express";
 import { functions, inngest } from "./lib/inngest.js";
+import {clerkMiddleware} from "@clerk/express"
+import { protectRoute } from "./middleware/protectRoute.js";
 const app = express();
 
 const __dirname=path.resolve();
 
 app.use(express.json());
 app.use(cors({origin: ENV.CLIENT_URL, credentials: true}));
+
+app.use(clerkMiddleware());
 
 app.use("/api/inngest", serve({client: inngest, functions}))
 app.get("/health", (req, res) => {
@@ -20,6 +24,10 @@ app.get("/health", (req, res) => {
 app.get("/books", (req, res) => {
     res.status(200).json({ message: "chal, World!" });
 });
+
+app.get("/video-calls", protectRoute ,(req,res)=>{
+    res.status(200).json({message: "video calls endpoint"});
+})
 
 if(ENV.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));

2253f7780cc61caf6910057e470a06712aceba79
aamish007 <aamishhussain7@gmail.com>
2026-04-05 15:42:18 +0530

    added 3rd party tools

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,18 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
 import path from "path";
+import cors from "cors";
+import { connectDB } from "./lib/db.js";
+import { serve } from "@inngest/express";
+import { functions, inngest } from "./lib/inngest.js";
 const app = express();
 
 const __dirname=path.resolve();
 
+app.use(express.json());
+app.use(cors({origin: ENV.CLIENT_URL, credentials: true}));
+
+app.use("/api/inngest", serve({client: inngest, functions}))
 app.get("/health", (req, res) => {
     res.status(200).json({ message: "bhak, World!" });
 });
@@ -18,7 +26,20 @@ if(ENV.NODE_ENV === "production"){
 
     app.get("/{*any}", (req, res) => {
         res.sendFile(path.join(__dirname,"../frontend","dist", "index.html" ));
-});
-app.listen(ENV.PORT, () => {
-    console.log("Server is running on port " + ENV.PORT);
 })};
+
+// app.listen(ENV.PORT, () => {
+//     console.log("Server is running on port " + ENV.PORT);
+//     connectDB();
+// });
+
+const startServer= async () =>{
+    try{
+        await connectDB();
+        app.listen(ENV.PORT, () => console.log("Server is running on port " + ENV.PORT));
+    }
+    catch(error){
+        console.error(`Error: ${error.message}`);
+    }
+}
+startServer();
